200af602f8c7a9c8c8e98179b4d8afe9838a7fe0
Dennis McCarthy <dmccuk@users.noreply.github.com>
2019-04-24 17:14:21 +0100

    python3 interpreter workaround

--- stack/files/standard.sh
+++ stack/files/standard.sh
@@ -2,3 +2,5 @@
 sudo apt update -yq
 sudo DEBIAN_FRONTEND=noninteractive apt upgrade -yq
 sudo apt install wget curl git python3-minimal -yq
+#fix for python3 symlink
+ln -s /usr/bin/python3.5 /usr/bin/python
