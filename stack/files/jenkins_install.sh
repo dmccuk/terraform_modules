015ee464265f6c6dbd94c3e617a3f4c9c1d7d010
Dennis McCarthy <dmccuk@users.noreply.github.com>
2019-04-30 21:39:48 +0100

    python3 symlink fix

--- stack/files/jenkins_install.sh
+++ stack/files/jenkins_install.sh
@@ -3,6 +3,9 @@
 sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo echo 'deb http://pkg.jenkins.io/debian-stable binary/' | sudo tee -a /etc/apt/sources.list.d/jenkins.list
 sudo apt-get update
+sudo apt install wget curl git python3-minimal -yq
+#fix for python3 symlink
+ln -s /usr/bin/python3.5 /usr/bin/python
 sudo apt-get -y install default-jre
 sudo apt-get -y install jenkins
 sudo systemctl start jenkins
