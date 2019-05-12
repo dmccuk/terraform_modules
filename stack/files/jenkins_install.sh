3ad1618960b3c55750b4719a666d95610fdd64d4
Dennis McCarthy <dmccuk@users.noreply.github.com>
2019-05-12 21:27:58 +0100

    Update jenkins_install.sh

--- stack/files/jenkins_install.sh
+++ stack/files/jenkins_install.sh
@@ -2,8 +2,9 @@
 # Installing Jenkins:
 sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo echo 'deb http://pkg.jenkins.io/debian-stable binary/' | sudo tee -a /etc/apt/sources.list.d/jenkins.list
+sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
-sudo apt install wget curl git python3-minimal -yq
+sudo apt install wget curl git python3-minimal ansible -yq
 #fix for python3 symlink
 ln -s /usr/bin/python3.5 /usr/bin/python
 sudo apt-get -y install default-jre
