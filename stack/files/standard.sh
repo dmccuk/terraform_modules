3c1b7711784bb97f0ac8c479c647efc987177090
Dennis McCarthy <dmccuk@users.noreply.github.com>
2019-07-01 13:50:43 +0100

    Update standard.sh

--- stack/files/standard.sh
+++ stack/files/standard.sh
@@ -3,4 +3,4 @@ sudo apt update -yq
 sudo DEBIAN_FRONTEND=noninteractive apt upgrade -yq
 sudo apt install wget curl git python3-minimal -yq
 #fix for python3 symlink
-ln -s /usr/bin/python3.5 /usr/bin/python
+#ln -s /usr/bin/python3.5 /usr/bin/python
