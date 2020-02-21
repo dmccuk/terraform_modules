16de0f539ebf4ae935447a8bc3ce0e4e3aa4221b
Dennis McCarthy <dmccuk@users.noreply.github.com>
2020-02-21 16:48:29 +0000

    Update standard.sh

--- stack/files/standard.sh
+++ stack/files/standard.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# sleep until instance is ready
+until [[ -f /var/lib/cloud/instance/boot-finished ]]; do
+  sleep 1
+done
+
 sudo apt update -yq
 sudo DEBIAN_FRONTEND=noninteractive apt upgrade -yq
 sudo apt install wget curl git python3-minimal -yq
