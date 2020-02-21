fb68d62b2b64aa284837f1e73c3fbfc6f71fa1a1
Dennis McCarthy <dmccuk@users.noreply.github.com>
2020-02-21 16:47:28 +0000

    Update gitlab_install.sh

--- stack/files/gitlab_install.sh
+++ stack/files/gitlab_install.sh
@@ -1,4 +1,10 @@
 #/bin/bash
+
+# sleep until instance is ready
+until [[ -f /var/lib/cloud/instance/boot-finished ]]; do
+  sleep 1
+done
+
 # Installing Gitlab:
 sudo apt update -yq
 sudo DEBIAN_FRONTEND=noninteractive apt upgrade -yq
