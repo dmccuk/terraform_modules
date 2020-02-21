202bbf723dbdc24b4f4defd93222f86dcc2cc7f0
Dennis McCarthy <dmccuk@users.noreply.github.com>
2020-02-21 16:47:47 +0000

    Update jenkins_install.sh

--- stack/files/jenkins_install.sh
+++ stack/files/jenkins_install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# sleep until instance is ready
+until [[ -f /var/lib/cloud/instance/boot-finished ]]; do
+  sleep 1
+done
+
 # Installing Jenkins:
 sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo echo 'deb http://pkg.jenkins.io/debian-stable binary/' | sudo tee -a /etc/apt/sources.list.d/jenkins.list
