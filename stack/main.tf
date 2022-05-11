f219bbf4bb0643a1e6b97ab9730bdbb32772ab45
Ubuntu <ubuntu@ip-172-31-11-5.eu-west-2.compute.internal>
2022-05-11 20:14:23 +0000

    updated to run on v1.1

--- stack/main.tf
+++ stack/main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
-  shared_credentials_file = "/root/.aws/credentials"
+# shared_credentials_file = "/root/.aws/credentials"
   region                  = var.region
-  version                 = "~> 2.41.0"
 }
 
 module "my_aws_key" {
@@ -58,7 +57,6 @@ module "ec2_med" {
   instance_count   = var.count_med
   instance_type    = "t2.medium"
   name_prefix      = "med"
-#  ami              = "ami-0f630a3f40b1eb0b8"
   ami              = var.ami
   provision_script = "files/standard.sh"
 }
@@ -88,8 +86,7 @@ module "jenkins" {
   instance_count   = var.count_jenkins
   instance_type    = "t2.micro"
   name_prefix      = "jenkins"
-#  ami              =  "ami-0f630a3f40b1eb0b8"
-  ami              =  "ami-01fe9ee78a93d794d"
+  ami              = var.ami
   provision_script = "files/jenkins_install.sh"
 }
 
--- stack/vars.tf
+++ stack/vars.tf
@@ -17,7 +17,7 @@ variable "count_jenkins" {
 #variable "access_key" {} # replaced by aws credentials
 #variable "secret_key" {} # replaced by aws credentials
 variable "region" {
-  default = "eu-west-3"
+  default = "eu-west-2"
 }
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
@@ -29,11 +29,5 @@ variable "server_port" {
 # ../modules/ec2/vars.tf
 
 variable "ami" {
-  default = "ami-0e0090d3db396e90d"
+  default = "ami-093762347983f511b"
 }
-#######################################
-# eu-west-3 images:
-# UBUNTU18.04 - ami-0e0090d3db396e90d
-# RHEL8 - ami-062fbc1f6aaecbede
-# RHEL7 - ami-072ec828dae86abe5
-#######################################
